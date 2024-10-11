089a50e6418708a443b9d797481be6f4ff3add07
Oksana <ksyubaranik@gmail.com>
2024-10-11 10:21:21 +0200

    Made website

---
+++ scripts.js
@@ -0,0 +1,26 @@
+// Menu burger
+const menuIcon = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+const menuLinks = document.querySelectorAll('.menu_list a');
+
+if (menuIcon) {
+    menuIcon.addEventListener("click", function(e) {
+        document.body.classList.toggle('_lock');
+        menuIcon.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
+}
+
+menuLinks.forEach(link => {
+    link.addEventListener("click", function() {
+        document.body.classList.remove('_lock');
+        menuIcon.classList.remove('_active');
+        menuBody.classList.remove('_active');
+    });
+});
+
+
+
+document.getElementById('bonus-button').addEventListener('click', function() {
+    alert('Вы получили бонус на Sweet Bonanza 1000!');
+});
